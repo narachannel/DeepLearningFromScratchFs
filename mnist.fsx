3a660a45313d1243b676e60d83b6091d858fb4b7
Koji Narazaki <koji@narachannel.com>
2017-05-04 23:36:19 +0900

    Forward and Softmax implemented

---
+++ mnist.fsx
@@ -0,0 +1,8 @@
+#load ".paket/load/main.group.fsx"
+
+open System
+open FSharp.Core
+open MathNet.Numerics
+open MathNet.Numerics.LinearAlgebra
+open XPlot.Plotly
+
--- neuralnetworkIntroduction.fsx
+++ neuralnetworkIntroduction.fsx
@@ -3,19 +3,41 @@
 open System
 open FSharp.Core
 open MathNet.Numerics
-open XPlot.Plotly;
+open MathNet.Numerics.LinearAlgebra
+open XPlot.Plotly
 
-let stepFunction (list : float list) =
-    List.map (fun x -> if x > 0. then 1.0 else 0.0 ) list
+let stepFunction v =
+    Vector.map (fun x -> if x > 0. then 1.0 else 0.0 ) v
 
-let x = [-5.0 .. 0.1 .. 5.0]
-//stepFunction x
+let x = vector [-5.0 .. 0.1 .. 5.0]
 
-let sigmoid (list : float list) = 
-    List.map (fun x -> 1. / (1. + exp(- x))) list
+let sigmoid (v : Vector<float>) = 
+    Vector.map (fun x -> 1. / (1. + exp(- x))) v
 
-let rectifiedLinearUnit (list : float list) =
-    List.map (fun x -> Math.Max(0., x)) list
-[Scatter(x = x, y = stepFunction x); 
-Scatter(x = x, y = sigmoid x);
-Scatter(x = x, y = rectifiedLinearUnit x)] |> Chart.Plot |> Chart.WithWidth 700 |> Chart.WithHeight 500 |> Chart.Show
+let rectifiedLinearUnit v =
+    Vector.map (fun x -> Math.Max(0., x)) v
+
+[Scatter(x = x, y = stepFunction x); Scatter(x = x, y = sigmoid x);Scatter(x = x, y = rectifiedLinearUnit x)] 
+|> Chart.Plot |> Chart.WithWidth 700 |> Chart.WithHeight 500 //|> Chart.Show
+
+let w1 = matrix [[0.1; 0.3; 0.5]; [0.2; 0.4; 0.6]]
+let w2 = matrix [[0.1; 0.4]; [0.2; 0.5]; [0.3; 0.6]]
+let w3 = matrix [[0.1; 0.3]; [0.2; 0.4]]
+
+let b1 = vector [0.1; 0.2; 0.3]
+let b2 = vector [0.1; 0.2]
+let b3 = vector [0.1; 0.2]
+
+let forward x = 
+    let a1 = x * w1 + b1
+    let z1 = sigmoid a1
+    let a2 = z1 * w2 + b2
+    let z2 = sigmoid a2
+    z2 * w3 + b3
+
+forward (vector [1.0; 0.5])
+
+let softmax (v : Vector<float>) =
+    let m = Vector.max v
+    let factor = Vector.toArray v |> Array.sumBy(fun x -> exp(x - m))
+    1. / factor * (Vector.map (fun x -> exp(x - m)) v)
